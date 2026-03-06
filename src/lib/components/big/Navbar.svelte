2fd3004e04a8c9931e0c94c8168004625e95c735
Ryan6712 <brustface32@gmail.com>
2026-03-06 13:14:57 +0700

    make center logo on navbar

--- src/lib/components/big/Navbar.svelte
+++ src/lib/components/big/Navbar.svelte
@@ -1,9 +1,17 @@
 <script>
 	import HomeButton from "$lib/components/smoll/HomeButton.svelte";
 	import SearchBar from "$lib/components/smoll/SearchBar.svelte";
+    import logoX from "/logoX.svg";
 </script>
 
-<nav class="flex justify-between flex-wrap px-5 py-3 sticky top-0 backdrop-blur-sm shadow-sm">
-    <HomeButton></HomeButton>
-    <SearchBar method="get" action="/"></SearchBar>
+<nav class="flex items-center flex-wrap px-5 py-3 sticky top-0 backdrop-blur-sm shadow-sm">
+    <div class="flex-1">
+        <HomeButton></HomeButton>
+    </div>
+
+    <img class="h-16" src={logoX} alt="Logo">
+
+    <div class="flex-1 flex justify-end">
+        <SearchBar method="get" action="/"></SearchBar>
+    </div>
 </nav>
